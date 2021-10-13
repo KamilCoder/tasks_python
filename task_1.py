761f67cb03702cb06562665e83cd7cc2733b379a
Kamil <59434156+KamilCoder@users.noreply.github.com>
2021-10-13 09:19:23 +0200

    changes

--- task_1.py
+++ task_1.py
@@ -5,14 +5,11 @@ def reverser():
         N_Reverse = int(str(N)[::-1])
     else:
         N_Reverse = -(int(str(abs(N))[::-1]))
-     # N to positive number
-     # convert to string to make reverse
 
-    if (N_Reverse >= -2147483649) and (N_Reverse <= 2147483647):
-        print(N_Reverse)
+    if -2147483649 <= N_Reverse <= 2147483647 :
+        return(N_Reverse)
     else:
-        print('0')
-    # This is simpe if reverse number is smaller  or bigger than
-    # signed integer range function returns
+        return('0')
+    
 if __name__ == "__main__":
     reverser()
